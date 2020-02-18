f78cbd5032a49be901cf76ff406ce9d20f914a1d
BKonstantin <costa.spy@yandex.ru>
2020-02-18 17:51:59 +0300

    commit pls

--- working_specialty.py
+++ working_specialty.py
@@ -4,12 +4,15 @@ from telebot import types
 bot = telebot.TeleBot('1054926363:AAFIizR6JDjoe4TJtmmocU0zIbiYtLYPWqA')
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Гуманитарное', 'Техническое', 'Не имеет значения')
+keyboard1.row('Гуманитарное', 'Техническое', 'Гум-тех', 'Показать направление')
 keyboard_answer = types.InlineKeyboardMarkup()
 key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
 keyboard_answer.add(key_yes)
 key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
 keyboard_answer.add(key_no)
+list_pf_spec = ['Гуманитарное', 'Техническое', 'Гум-тех']
+flag = list_pf_spec[2]
+num = -1
 
 
 @bot.message_handler(commands=['start'])
@@ -19,26 +22,35 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+    global num
+    global flag
     if message.text.lower() == 'гуманитарное':
+        num = 0
         bot.send_message(message.chat.id, 'Вы уверены что хотите выбрать гуманитарное направление?',
                          reply_markup=keyboard_answer)
     elif message.text.lower() == 'техническое':
+        num = 1
         bot.send_message(message.chat.id, 'Вы уверены что хотите выбрать техническое направление?',
                          reply_markup=keyboard_answer)
-    elif message.text.lower() == 'не имеет значения':
+    elif message.text.lower() == 'гум-тех':
+        flag = list_pf_spec[num]
         bot.send_message(message.chat.id, 'Хорошо, вам будут приходить все новости')
     elif message.text.lower() == 'я тебя люблю':
         bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+    elif message.text.lower() == 'показать направление':
+        bot.send_message(message.chat.id, flag)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
+    global flag
     if call.data == "yes":
-        bot.send_message(call.message.chat.id, 'Хорошо, вам будут приходить новости по вашему направлению');
+        flag = list_pf_spec[num]
+        bot.send_message(call.message.chat.id, 'Хорошо, вам будут приходить новости по вашему направлению')
     elif call.data == "no":
         bot.send_message(call.message.chat.id, 'Какое направление вы хотите выбрать',
                          reply_markup=keyboard1)
-    bot.delete_message(call.message.chat.id, call.message.chat.id)
+    bot.delete_message(call.message.chat.id, call.message.message_id)
 
 
 @bot.message_handler(content_types=['sticker'])
@@ -48,5 +60,5 @@ def sticker_id(message):
 
 try:
     bot.polling(none_stop=True, interval=0)
-except Exception:
-    pass
+except Exception as er:
+    print(er)
